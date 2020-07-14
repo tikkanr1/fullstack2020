87781bdbc889cddb8d50e964454842bc90079411
Riku Tikkanen <riku.tikkanen@aalto.fi>
2020-07-14 20:49:57 +0300

    anekdootit step3

--- osa1/anekdootit/src/index.js
+++ osa1/anekdootit/src/index.js
@@ -3,20 +3,34 @@ import ReactDOM from 'react-dom'
 
 const App = (props) => {
   const [selected, setSelected] = useState(0)
-  const points = { 0:1, 1: 3, 2: 4, 3: 2 }
+  const [votes, setVotes] = useState([0, 0, 0, 0, 0, 0, 0])
+  const best = votes.indexOf(Math.max(...votes))
+
+  const vote = () => {
+    const copyVotes = [...votes]
+    copyVotes[selected] += 1
+    setVotes(copyVotes)
+  }
 
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       <p>{props.anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <button onClick={vote}>
+        vote
+      </button>
       <button onClick={() => setSelected(Math.floor((Math.random() * 6) +0))}>
         next anecdote
       </button>
+      <h1>Anecdote with most votes</h1>
+      <p>{props.anecdotes[best]}</p>
     </div>
   )
 }
 
 const anecdotes = [
-  'If it hurts, do it more often',
+  'If it hurts, do it more often.',
   'Adding manpower to a late software project makes it later!',
   'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
