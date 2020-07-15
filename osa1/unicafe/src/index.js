440c23ffc875c35f5206e6c86ecfab419ae4d0e0
Riku Tikkanen <riku.tikkanen@aalto.fi>
2020-07-15 12:03:24 +0300

    kurssitiedot step7

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -6,9 +6,9 @@ const Statistics = ({good, neutral, bad}) => {
     return(
       <table>
         <StatisticLine text="good" value={good}/>
-        <StatisticLine text="neutral" value={neutral}/> 
+        <StatisticLine text="neutral" value={neutral}/>
         <StatisticLine text="bad" value={bad}/>
-        <StatisticLine text="all" value={good + neutral + bad}/>       
+        <StatisticLine text="all" value={good + neutral + bad}/>
         <StatisticLine text="average" value={(good - bad) / (good + neutral + bad)}/>
         <StatisticLine text="positive" value={(good / (good + neutral + bad) * 100)+" %"}/>
       </table>
@@ -25,7 +25,7 @@ const StatisticLine = (props) => {
     <tr>
       <td>{props.text}</td>
       <td>{props.value}</td>
-    </tr> 
+    </tr>
   )
 }
 
@@ -54,6 +54,6 @@ const App = () => {
   )
 }
 
-ReactDOM.render(<App />, 
+ReactDOM.render(<App />,
   document.getElementById('root')
 )
---
+++ osa2/kurssitiedot/src/index.js
@@ -0,0 +1,85 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <li style={{ listStyleType: "none" }}>
+          {props.parts.map(part => 
+            <Part name={part.name} exercises={part.exercises}/>
+          )}
+      </li>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>{props.name} {props.exercises}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+    </div>
+  )
+}
+
+const Course = (props) => {
+  return (
+    <div>
+      <Header course={props.course.name}/>
+      <Content parts={props.course.parts}/>
+      <Total parts={props.course.parts}/>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+      { name: 'Redux',
+        exercises: 11,
+        id: 4
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course}/>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
