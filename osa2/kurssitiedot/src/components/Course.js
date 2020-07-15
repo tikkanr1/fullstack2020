7086ba04e79b31abb01c1f7206ac45938472a6ef
Riku Tikkanen <riku.tikkanen@aalto.fi>
2020-07-15 16:18:01 +0300

    kurssitiedot Course moduuli

---
+++ osa2/kurssitiedot/src/components/Course.js
@@ -0,0 +1,57 @@
+import React from "react";
+  
+  const Course = (props) => {
+    return (
+      <div>
+        {props.courses.map(course => 
+            <ul key={course.id}>
+                <Header course={course.name}/>
+                <Content parts={course.parts}/>
+                <Total parts={course.parts}/>
+            </ul>)}
+      </div>
+    )
+  }
+  
+  const Header = (props) => {
+    return (
+      <div>
+        <h2>{props.course}</h2>
+      </div>
+    )
+  }
+  
+  const Content = (props) => {
+    console.log("content",props)
+    return (
+      <div>
+        {props.parts.map(part => 
+          <li key={part.id}>
+            <Part name={part.name} exercises={part.exercises}/>
+          </li>
+        )}
+      </div>
+    )
+  }
+  
+  const Part = (props) => {
+    return (
+      <div>
+        <p>{props.name} {props.exercises}</p>
+      </div>
+    )
+  }
+  
+  const Total = (props) => {
+    const sum = props.parts.reduce(
+      (acc, cur) => acc + cur.exercises, 0
+    )
+  
+    return (
+      <div>
+        <p>total of {sum} exercises</p>
+      </div>
+    )
+  }
+
+  export default Course;
--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -1,83 +1,74 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Course from "./components/Course"
 
-const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.course}</h1>
-    </div>
-  )
-}
-
-const Content = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <li style={{ listStyleType: "none" }}>
-          {props.parts.map(part => 
-            <Part name={part.name} exercises={part.exercises}/>
-          )}
-      </li>
-    </div>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <div>
-      <p>{props.name} {props.exercises}</p>
-    </div>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <div>
-      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
-    </div>
-  )
-}
-
-const Course = (props) => {
-  return (
-    <div>
-      <Header course={props.course.name}/>
-      <Content parts={props.course.parts}/>
-      <Total parts={props.course.parts}/>
-    </div>
-  )
-}
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      { name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'Intredasting',
+          exercises: 410,
+          id: 3
+        }
+      ]
+    }
+    ,
+    {
+      name: 'Bonus Course',
+      id: 3,
+      parts: [
+        {
+          name: 'Bonus round',
+          exercises: 6,
+          id: 1
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
-      <Course course={course}/>
+      <Course courses={courses}/>
     </div>
   )
 }
